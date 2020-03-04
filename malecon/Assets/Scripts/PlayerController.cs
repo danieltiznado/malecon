d54e88a6b027f99bc553d7ab3b080878417d6a26
danielsarabia <daniel.sarabia.tiznado@gmail.com>
2020-03-03 22:03:35 -0600

    - Add two sprite animations for the player: walking and clapping. - Add a basic controller movement script to move the player left or right and clap. - Added animator to set the flow of the walking and clapping animations. - On controller script, trigger clap animator if clap button is pressed.

--- malecon/Assets/Scripts/PlayerController.cs
+++ malecon/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // What is the maximum speed we want the player to walk at
+    public float maxSpeed = 5f;
+
+    // Start facing right (like the sprite-sheet)
+    private bool facingRight = true;
+
+    // This will be a reference to the RigidBody2D 
+    // component in the Player object
+    public Rigidbody2D rb;
+
+    // This is a reference to the Animator component
+    public Animator anim;
+
+    // If the player is clapping
+    private bool clap = false;
+
+    private void Update()
     {
-        
+        // If clap button is pressed, set clap to true
+        if (Input.GetButtonDown("Clap"))
+        {
+            //anim.SetTrigger("Clap");
+            clap = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // We use FixedUpdate to do all the animation work
+    void FixedUpdate()
     {
-        
+
+        // Get the extent to which the player is currently pressing left or right
+        float h = Input.GetAxis("Horizontal");
+
+        // Move the RigidBody2D (which holds the player sprite)
+        // on the x axis based on the stae of input and the maxSpeed variable
+        rb.velocity = new Vector2(h * maxSpeed, rb.velocity.y);
+
+        // If clap bool is true, trigger the clapping animation
+        // on the animator
+        if (clap)
+        {
+            anim.SetTrigger("Clap");
+        }
+        // We reset the clap bool to false
+        clap = false;
+
+        // Check which way the player is facing 
+        // and call reverseImage if neccessary
+        if (h > 0 && !facingRight)
+            reverseImage();
+        else if (h < 0 && facingRight)
+            reverseImage();
+
+    }
+
+    void reverseImage()
+    {
+        // Switch the value of the Boolean
+        facingRight = !facingRight;
+
+        // Get and store the local scale of the RigidBody2D
+        Vector2 theScale = rb.transform.localScale;
+
+        // Flip it around the other way
+        theScale.x *= -1;
+        rb.transform.localScale = theScale;
     }
 }
